93f1c86497f0ac7d1e5bf39ca870307a7c80aaf0
OrangeCat0517 <30237108+OrangeCat0517@users.noreply.github.com>
2021-11-25 12:10:52 +0800

    这是一个完整的例子

---
+++ src/test/java/com/example/mybatis/mapper/UserMapperTest.java
@@ -0,0 +1,77 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.domain.Sex;
+import com.example.mybatis.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest {
+    private SqlSession sqlSession; //类似于JDBC中的Connection，数据库链接
+    private UserMapper userMapper;
+    @BeforeEach
+    void setUp() throws IOException {
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+        //通过读取MyBatis配置文件可以生成SqlSessionFactory，这个对象表示数据库的所有关键信息
+        sqlSession = sqlSessionFactory.openSession(); //通过SqlSessionFactory获得一个打开的数据库连接
+        userMapper = sqlSession.getMapper(UserMapper.class);//mybatis利用映射文件帮我们生成了这个接口的实现类
+    }
+
+    @AfterEach
+    void tearDown() {
+        sqlSession.close(); //关闭数据库链接
+    }
+
+    @Test
+    void save() {
+        User user = new User();
+        user.setEmail("tom@google.com");
+        user.setPassword("123456");
+        user.setBirthday(LocalDate.now());
+        user.setSex(Sex.M);
+        user.setLoginCount(0);
+        user.setLastLoginTime(null);
+        userMapper.save(user);
+        sqlSession.commit(); //提交事务，增删该都需要提交事物才能生效
+    }
+
+    @Test
+    void deleteById() {
+        userMapper.deleteById(2);
+        sqlSession.commit();
+    }
+
+    @Test
+    void updateById() {
+        User user = new User();
+        user.setId(3);
+        user.setEmail("jerry@google.com");
+        user.setPassword("654321");
+        user.setBirthday(LocalDate.now());
+        user.setSex(Sex.M);
+        user.setLoginCount(0);
+        user.setLastLoginTime(null);
+        userMapper.updateById(user);
+        sqlSession.commit(); //提交事务，增删该都需要提交事物才能生效
+    }
+
+    @Test
+    void findOne() {
+        System.out.println(userMapper.findOne(3));
+    }
+
+    @Test
+    void findAll() {
+        userMapper.findAll().forEach(System.out::println);
+    }
+}
